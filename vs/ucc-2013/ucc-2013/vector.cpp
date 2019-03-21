6ea1a593ec967d83fdaa96d71bdaa889ebf8b557
k91191 <2801673893@qq.com>
2019-03-21 21:45:46 +0800

    second commit

---
+++ vs/ucc-2013/ucc-2013/vector.cpp
@@ -0,0 +1,33 @@
+#include "ucl.h"
+
+/**
+* Create a vector with the given size
+* @param size vector's size
+*/
+Vector CreateVector ( int size )
+{
+	Vector v = NULL;
+	// allocate a struct vector object from Heap
+	ALLOC ( v,vector* );
+
+	// allocate @size slots for the vector object
+	v->data = ( void ** )HeapAllocate ( CurrentHeap, size * sizeof ( void * ) );
+	v->size = size;
+	v->len = 0;
+	return v;
+}
+
+/**
+* Double the vector's size to hold more data
+*/
+//  Anytime, the vector is not large enough, we expand it.
+void ExpandVector ( Vector v )
+{
+	void *orig;
+
+	orig = v->data;
+	v->data = ( void** )HeapAllocate ( CurrentHeap, v->size * 2 * sizeof ( void * ) ); //这里与源代码不符
+	memcpy ( v->data, orig, v->size * sizeof ( void * ) );
+	v->size *= 2;
+}
+
